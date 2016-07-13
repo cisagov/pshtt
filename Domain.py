f399cde0a5dc4c2d9ed7c181f694db387046b783
KyleEvers <kyleevers14@yahoo.com>
2016-07-13 15:53:15 -0400

    Update Domain.py

--- Domain.py
+++ Domain.py
@@ -5,9 +5,10 @@ import urllib2
 class Domain:
 
     def __init__(self, base_domain):
-        self.domain = "http://" + base_domain
+        self.domain = ""
         self.base_domain = base_domain
         self.canonical = ""
+        self.https_domain = ""
         self.live = ""
         self.redirect = ""
         self.redirect_to = ""
@@ -30,6 +31,7 @@ class Domain:
         return ("Domain: " + str(self.domain) + " \n" +
                 "Base Domain: " + str(self.base_domain) + "\n"+
                 "Canonical: " + self.canonical + "\n"+
+                "HTTPS Domain:" + self.https_domain + "\n" +
                 "Live: " + self.live + "\n"+
                 "Redirect: " + self.redirect + "\n"+
                 "Redirect To: " + self.redirect_to + "\n" +
@@ -37,6 +39,7 @@ class Domain:
                 "Defaults HTTPS: " + self.defaults_to_https + "\n" +
                 "Downgrades HTTPS: " + self.downgrades_https + "\n" +
                 "Strictly Forces HTTPS: " + self.strictly_forces_https + "\n" +
+                "HTTPS Bad Chain: " + self.https_bad_chain + "\n" +
                 "HTTPS Bad Host Name: " + self.https_bad_hostname + "\n" +
                 "HSTS: " + self.hsts + "\n" +
                 "HSTS Header: " + self.hsts_header + "\n" +
@@ -46,3 +49,29 @@ class Domain:
                 "HSTS Preloaded: " + self.hsts_preloaded + "\n" +
                 "Broken Root: " + self.broken_root + "\n" +
                 "Broken www: " + self.broken_www + "\n")
+
+    def output_to_csv(self):
+        return (str(self.domain) + "," +
+                str(self.base_domain) + ","+
+                self.canonical + ","+
+                self.live + ","+
+                self.redirect + ","+
+                self.redirect_to + "," +
+                self.https_domain + "," +
+                self.valid_https + "," +
+                self.defaults_to_https + "," +
+                self.downgrades_https + "," +
+                self.strictly_forces_https + "," +
+                self.https_bad_chain + "," +
+                self.https_bad_hostname + "," +
+                self.hsts + "," +
+                self.hsts_header + "," +
+                self.hsts_max_age + "," +
+                self.hsts_all_subdomains + "," +
+                self.hsts_preload_ready + "," +
+                self.hsts_preloaded + "," +
+                self.broken_root + "," +
+                self.broken_www + "\n")
+
+
+
