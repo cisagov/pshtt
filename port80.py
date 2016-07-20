d008358689b021851d37b49ca2923603fa786e75
KyleEvers <kyleevers14@yahoo.com>
2016-07-20 15:36:42 -0400

    Create port80.py

---
+++ port80.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import httplib
+import urllib2
+
+class port80:
+
+    def __init__(self, domain, base_domain):
+        self.domain = domain
+        self.base_domain = base_domain
+        self.canonical = ""
+        self.live = False
+        self.redirect = False
+        self.redirect_to = ""
