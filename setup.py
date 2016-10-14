e6a380b4a41f5bab75febfb574152f13a828c0d4
h-m-f-t <cameron@hmft.xyz>
2016-10-14 01:30:14 -0400

    bump to 0.1.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='pshtt',
 
     # Versions should comply with PEP440
-    version='0.1.0',
+    version='0.1.1',
     description='Scan websites for HTTPS deployment best practices',
 
     # NCATS "homepage"
