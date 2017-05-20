97843e21d1dc75bcfd6640d6a306ef5f9c002202
Cameron Dixon <h-m-f-t@users.noreply.github.com>
2017-05-20 13:48:41 -0400

    bump to 0.1.6

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='pshtt',
 
     # Versions should comply with PEP440
-    version='0.1.5',
+    version='0.1.6',
     description='Scan websites for HTTPS deployment best practices',
 
     # NCATS "homepage"
