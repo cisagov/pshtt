d5ca0ad5e1681d374ea0502a2fbdacca332dc64e
Ariana Mirian <arianamirian28@gmail.com>
2017-08-09 15:45:56 -0700

    Changed syntax to look cleaner

--- pshtt/pshtt.py
+++ pshtt/pshtt.py
@@ -286,9 +286,7 @@ def basic_check(endpoint):
             base_immediate = parent_domain_for(subdomain_immediate)
 
             endpoint.redirect_immediately_to = immediate
-            endpoint.redirect_immediately_to_www = re.match(r'^https?://www\.', immediate)
-            if endpoint.redirect_immediately_to_www is not None:
-                endpoint.redirect_immediately_to_www = True
+            endpoint.redirect_immediately_to_www = (re.match(r'^https?://www\.', immediate) is not None)
             endpoint.redirect_immediately_to_https = immediate.startswith("https://")
             endpoint.redirect_immediately_to_http = immediate.startswith("http://")
             endpoint.redirect_immediately_to_external = (base_original != base_immediate)
