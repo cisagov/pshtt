0b81c5689122bd8a31e9a8107861b1880eee781f
Ariana Mirian <arianamirian28@gmail.com>
2017-08-11 11:43:52 -0700

    updated comment

--- pshtt/pshtt.py
+++ pshtt/pshtt.py
@@ -34,7 +34,7 @@ WEB_CACHE = None
 # Default, overrideable via --user-agent
 USER_AGENT = "pshtt, https scanning"
 
-# Defaults to 1 second, overrideable via --timeout
+# Defaults to 10 second, overrideable via --timeout
 TIMEOUT = 10
 
 # The fields we're collecting, will be keys in JSON and
