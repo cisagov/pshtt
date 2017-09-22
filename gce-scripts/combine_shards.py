a3ed7e4bf43fa90135a141d30587e4761cdf9e09
Ariana Mirian <arianamirian28@gmail.com>
2017-09-22 08:34:29 -0700

    Scripts to run pshtt on GCE

---
+++ gce-scripts/combine_shards.py
@@ -0,0 +1,28 @@
+"""Combines pshtt shards into one final data file."""
+import json
+import sys
+
+
+def main():
+  if (len(sys.argv)) < 2:
+    print 'you need a filename!'
+    exit(1)
+  # Master file is the file with the list of filenames to intake.
+  # Fileception.
+  master_file = sys.argv[1]
+  filenames = []
+
+  # Read in the filenames that are the different shards.
+  with open(master_file, 'r') as input_file:
+    for line in input_file:
+      filenames.append(line.rstrip())
+  # For each shard, read it in and append to the final list to
+  # print out.
+  for item in filenames:
+    with open(item, 'r') as input_file:
+      json_data = json.load(input_file)
+      for item in json_data:
+        print json.dumps(item)
+
+if __name__ == '__main__':
+  main()
---
+++ gce-scripts/running_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Runs pshtt with a 10 second time, "Google-Transparency-Report" as the user
+# agent, with roots.pem as the CA file, and debug on. Logging goes to
+# time_<input_file_name>.txt
+
+# ./running_script.sh test_file.csv
+# output files: test_file.csv.json, time_test_file.csv.txt
+
+input_file=$1
+(time python3 -m pshtt.cli "${input_file}" -t 10 -u "Google-Transparency-Report" -j -o "${input_file}".json -f "roots.pem" --debug) 2> time_"${input_file}".txt
