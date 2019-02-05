b9dfad9fd83da62b4baa0ecf6df4e5487a8a5211
Shane Frasier <jeremy.frasier@beta.dhs.gov>
2019-02-05 11:30:53 -0500

    flake8 F632 says we must use ==/!= to compare string literals

--- pshtt/utils.py
+++ pshtt/utils.py
@@ -37,7 +37,7 @@ def json_for(object):
 
 def write(content, destination, binary=False):
     parent = os.path.dirname(destination)
-    if parent is not "":
+    if parent != "":
         mkdir_p(parent)
 
     if binary:
