1492deecea3c8833279ad860717e4e27601d8af5
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-02-27 13:42:17 -0500

    Make shellcheck happy

--- bump_version.sh
+++ bump_version.sh
@@ -1,4 +1,4 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
 # bump_version.sh (show|major|minor|patch|prerelease|build)
 
@@ -8,14 +8,14 @@ HELP_INFORMATION="bump_version.sh (show|major|minor|patch|prerelease|build|final
 
 old_version=$(sed -n "s/^__version__ = '\(.*\)'$/\1/p" $VERSION_FILE)
 
-if [[ $# -ne 1 ]]
+if [ $# -ne 1 ]
 then
-   echo $HELP_INFORMATION
+   echo "$HELP_INFORMATION"
 else
     case $1 in
         major|minor|patch|prerelease|build)
             new_version=$(python -c "import semver; print(semver.bump_$1('$old_version'))")
-            echo Changing version from $old_version to $new_version
+            echo Changing version from "$old_version" to "$new_version"
             sed -i "s/$old_version/$new_version/" $VERSION_FILE
             git add $VERSION_FILE
             git commit -m"Bump version from $old_version to $new_version"
@@ -23,17 +23,17 @@ else
             ;;
         finalize)
             new_version=$(python -c "import semver; print(semver.finalize_version('$old_version'))")
-            echo Changing version from $old_version to $new_version
+            echo Changing version from "$old_version" to "$new_version"
             sed -i "s/$old_version/$new_version/" $VERSION_FILE
             git add $VERSION_FILE
             git commit -m"Finalize version from $old_version to $new_version"
             git push
             ;;
         show)
-            echo $old_version
+            echo "$old_version"
             ;;
         *)
-            echo $HELP_INFORMATION
+            echo "$HELP_INFORMATION"
             ;;
     esac
 fi
--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -o nounset
+set -o errexit
+set -o pipefail
+
 echo "$DOCKER_PW" | docker login -u "$DOCKER_USER" --password-stdin
 version=$(./bump_version.sh show)
-docker push "$IMAGE_NAME":$version
+docker push "$IMAGE_NAME":"$version"
