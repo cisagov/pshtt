ad97fe37285471d8dc689c50a3f7cc27ea22e101
Felddy <markf+github@geekpad.com>
2019-04-11 15:14:16 -0400

    Fix deprication of `warn`
    DeprecationWarning: The 'warn' function is deprecated, use 'warning' 
    instead

--- example/example.py
+++ example/example.py
@@ -22,7 +22,7 @@ def example():
     """A dummy function."""
     logging.debug("This is a debug message")
     logging.info("This is an info message")
-    logging.warn("This is a warning message")
+    logging.warning("This is a warning message")
     logging.error("This is an error message")
     logging.critical("This is a critical message")
 
