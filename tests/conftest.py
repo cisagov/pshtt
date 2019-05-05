7394753dff8ac5e4c5c033f3b679358577eb4d5a
Felddy <markf+github@geekpad.com>
2019-05-05 14:24:16 -0400

    Add sample conftest file.

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+"""pytest plugin configuration.
+
+https://docs.pytest.org/en/latest/writing_plugins.html#conftest-py-plugins
+"""
+import pytest
+
+
+def pytest_addoption(parser):
+    """Add new commandline options to pytest."""
+    parser.addoption(
+        "--runslow", action="store_true", default=False, help="run slow tests"
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    """Modify collected tests based on custom marks and commandline options."""
+    if config.getoption("--runslow"):
+        # --runslow given in cli: do not skip slow tests
+        return
+    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    for item in items:
+        if "slow" in item.keywords:
+            item.add_marker(skip_slow)
