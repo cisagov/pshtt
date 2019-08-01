d158b06dde204d125adce13932cf24e9b48d3565
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-08-01 11:31:59 -0400

    Update field name
    The CertificateInfoScanResult class in sslyze had some fields renamed
    in version 2.1.1.

--- pshtt/pshtt.py
+++ pshtt/pshtt.py
@@ -740,7 +740,7 @@ def https_check(endpoint):
         ):
             # *** TODO check that it is not a bad hostname and that the root cert is trusted before suggesting that it is an intermediate cert issue.
             endpoint.https_missing_intermediate_cert = True
-            if(cert_plugin_result.successful_trust_store is None):
+            if(cert_plugin_result.verified_certificate_chain is None):
                 logging.warning("{}: Untrusted certificate chain, probably due to missing intermediate certificate.".format(endpoint.url))
                 utils.debug("{}: Only {} certificates in certificate chain received.".format(endpoint.url, cert_plugin_result.received_certificate_chain.__len__()))
             elif(custom_trust is True and public_trust is False):
@@ -750,7 +750,7 @@ def https_check(endpoint):
                         cert_plugin_result = None
                         command = sslyze.plugins.certificate_info_plugin.CertificateInfoScanCommand(ca_file=PT_INT_CA_FILE)
                         cert_plugin_result = scanner.run_scan_command(server_info, command)
-                        if(cert_plugin_result.successful_trust_store is not None):
+                        if(cert_plugin_result.verified_certificate_chain is not None):
                             public_trust = True
                             endpoint.https_public_trusted = public_trust
                             logging.warning("{}: Trusted by special public trust store with intermediate certificates.".format(endpoint.url))
