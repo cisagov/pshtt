d59f5f97cdfee079a817263fae1d7f797941a77b
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2020-02-16 15:38:30 -0500

    Change coveralls pin to avoid the version with the regression. 1.11.1 is released which fixed the regression introduced in 1.11.0 per https://github.com/coveralls-clients/coveralls-python/releases/tag/1.11.1

--- setup.py
+++ setup.py
@@ -76,12 +76,12 @@ setup(
         "test": [
             "pre-commit",
             # coveralls 1.11.0 added a service number for calls from
-            # GitHub Actions. When run this resulted in a 422 response from the
-            # coveralls API with the message:
+            # GitHub Actions. This caused a regression which resulted in a 422
+            # response from the coveralls API with the message:
             # Unprocessable Entity for url: https://coveralls.io/api/v1/jobs
-            # Temporarily pinning coveralls to before this version until this
-            # issue is resolved.
-            "coveralls @ git+https://github.com/coveralls-clients/coveralls-python.git@fix-github-actions",
+            # 1.11.1 fixed this issue, but to ensure expected behavior we'll pin
+            # to never grab the regression version.
+            "coveralls != 1.11.0",
             "coverage",
             "pytest-cov",
             "pytest",
