3dac9898ee7416aac9611842315175f4c7755fad
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-02-03 10:10:54 -0500

    Add Python 3.10 to package classifiers
    This was missed when we added testing/building for Python 3.10.

--- setup.py
+++ setup.py
@@ -78,6 +78,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     python_requires=">=3.6",
     # What does your project relate to?
